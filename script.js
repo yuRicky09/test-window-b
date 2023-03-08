f20fd963e198ed3c6779bbe2485a7ab19b12bdd3
Yu Ricky <leoleo5994@gmail.com>
2023-03-08 18:28:01 +0800

    update js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 window.addEventListener("message", (event) => {
   // IMPORTANT: check the origin of the data!
-  console.log(event.data);
-  // if (event.origin === "https://test-window-a.vercel.app/") {
-  //   // The data was sent from your site.
-  //   // Data sent with postMessage is stored in event.data:
-  // } else {
-  //   // The data was NOT sent from your site!
-  //   // Be careful! Do not use it. This else branch is
-  //   // here just for clarity, you usually shouldn't need it.
-  //   return;
-  // }
+  console.log(event.origin);
+  if (event.origin === "https://test-window-a.vercel.app/") {
+    console.log(event.data);
+    // The data was sent from your site.
+    // Data sent with postMessage is stored in event.data:
+  } else {
+    // The data was NOT sent from your site!
+    // Be careful! Do not use it. This else branch is
+    // here just for clarity, you usually shouldn't need it.
+    return;
+  }
 });
