bb1193ec59a92ef1550571ee1eca2330f1598c36
Yu Ricky <leoleo5994@gmail.com>
2023-03-08 17:55:12 +0800

    update js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const loca = document.querySelector("#location");
 loca.innerText = window.parent.location.ancestorOrigins;
 console.log(loca);
+
+window.addEventListener("message", (event) => {
+  // IMPORTANT: check the origin of the data!
+  if (event.origin === "https://test-window-a.vercel.app/") {
+    // The data was sent from your site.
+    // Data sent with postMessage is stored in event.data:
+    console.log(event.data);
+  } else {
+    // The data was NOT sent from your site!
+    // Be careful! Do not use it. This else branch is
+    // here just for clarity, you usually shouldn't need it.
+    return;
+  }
+});
